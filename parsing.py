2caf3e1608a212583c4fce2f52e456d64f897470
aymene <aymene98@icloud.com>
2021-02-28 23:26:04 +0100

    adding parsing

---
+++ parsing.py
@@ -0,0 +1,32 @@
+import nltk
+from nltk import parse
+from nltk import CFG
+import tagging
+from nltk.parse import ShiftReduceParser
+
+# readding grammar
+def get_grammar_path(path):
+    return nltk.data.load(path)
+
+def get_grammar_string(string):
+    return CFG.fromstring(string)
+
+# splitting the text into sentences then tagging each sentence.
+def split_text_to_sents(text):
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    sentences = [sentence for sentence in sent_detector.tokenize(text)]
+    tags_and_sents = []
+    for sent in sentences:
+        tags_and_sents.append((sent, tagging.pos_tags(sent)))
+    return tags_and_sents
+
+# using the grammar and the POS-tags that we got from the previous functions 
+# we try to find at least a parsing tree.
+def parse(grammar, text, RDP=True):
+    parser = nltk.RecursiveDescentParser(grammar)
+    if not RDP:
+        parser = ShiftReduceParser(grammar)
+    
+    tags_and_sents = split_text_to_sents(text)
+    sents_and_trees = [(sent[0], sent[1], parser.parse(sent[1]) for sent in tags_and_sents]
+    return sents_and_trees # contains tuples (words, tags, trees)
--- tagging.py
+++ tagging.py
@@ -2,22 +2,22 @@ import nltk, re, pickle
 from reading_collection import get_corpus, get_sentences_word_pos
 
 def tagger(taged_sents):
-    t1 = nltk.UnigramTagger(taged_sents)
+    t0 = nltk.DefaultTagger('NN')
+    t1 = nltk.UnigramTagger(taged_sents, backoff=t0)
     t2 = nltk.BigramTagger(taged_sents, backoff=t1)
     t3 = nltk.NgramTagger(3, taged_sents, backoff=t2)
     t4 = nltk.NgramTagger(4, taged_sents, backoff=t3)
     return t4
 
 def create_tagger():
-        
     corpus = get_corpus(number_of_files=1000)
     word_pos_sentences = get_sentences_word_pos(corpus)
-    tagger = tagger(word_pos_sentences)
-    pickle.dump(tagger, open( "tagger.p", "wb"))
+    t = tagger(word_pos_sentences)
+    pickle.dump(t, open( "tagger.p", "wb"))
     
-    sent = 'Hello, my name is aymene.'
-    l = re.findall(r"[\w']+|[.,!?;]", sent)
-    print("itwearks x)",tagger.tag(l))
+    #sent = 'Hello, my name is aymene.'
+    #l = re.findall(r"[\w']+|[.,!?;]", sent)
+    #print("itwearks x)",t.tag(l))
 
 def tag(sent):
     tagger = pickle.load(open("tagger.p", "rb"))
@@ -27,4 +27,4 @@ def tag(sent):
 def pos_tags(sent):
     return [t[1] for t in tag(sent)]
     
-print(pos_tags('Hello, my name is aymene.'))
+#l = pos_tags('Hello, my name is aymene.')
